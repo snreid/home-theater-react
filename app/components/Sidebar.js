a13edf324cc398a1972fe3bc875c5ba79e2660a7
Sarah Reid <sreid@covermymeds.com>
2017-02-17 14:41:43 -0500

    Remove button

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -8,13 +8,9 @@ const Sidebar = ({ displayingDvd, sidebar }) => {
   return (
     <div id='sidebar-right' className="sidebar collapsed col-md-3 ">
       <div className='well affix'>
-      <div className='row'>
-        <div className='col-md-12'>
-          <button className='btn btn-default pull-right' onClick={closeSidebar}>
-            x
-          </button>
-        </div>
-      </div>
+        <span className='pull-right' onClick={closeSidebar}>
+          X
+        </span>
         <AddDvd shouldDisplay={ sidebar == Sidebars.ADD_DVD }/>
         <DvdDetails dvd={displayingDvd} shouldDisplay={sidebar == Sidebars.SHOW_DVD} />
       </div>
