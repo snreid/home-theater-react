0eb73eef349733f4ecf2dd7e8c9a37a845c680fb
Sarah Reid <sreid@covermymeds.com>
2017-02-17 14:48:14 -0500

    More DVD details

--- app/containers/DvdDetails.js
+++ app/containers/DvdDetails.js
@@ -2,7 +2,17 @@ import React, { PropTypes } from 'react'
 
 const DvdDetails = ({ dvd, shouldDisplay }) =>(
   shouldDisplay &&
-    <h2>{ dvd.DVD_Title }</h2>
+    <div>
+      <h2>{ dvd.DVD_Title }</h2>
+      <ul className='list-group'>
+        <li className='list-group-item'>
+          <strong>Genre:</strong> {dvd.Genre}
+        </li>
+        <li className='list-group-item'>
+          <strong>Release Date:</strong> {dvd.DVD_ReleaseDate}
+        </li>
+      </ul>
+    </div>
 )
 
 export default DvdDetails
