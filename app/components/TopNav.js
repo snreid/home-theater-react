b048c6e3f0ee00a68bb31198296c9a78e276c4d0
Sarah Reid <sreid@covermymeds.com>
2017-02-20 12:20:39 -0500

    Add alt text to buttons

--- app/components/TopNav.js
+++ app/components/TopNav.js
@@ -3,10 +3,10 @@ import React, { PropTypes } from 'react'
 const TopNav = ({ addDvd, openQuickScan }) => (
   <nav id='main-nav' className='navbar navbar-default navbar-fixed-top'>
     <div className='container'>
-      <button className='btn btn-default navbar-btn navbar-right' onClick={addDvd}>
-          <span className='glyphicon glyphicon-film' aria-hidden='true'></span>
+      <button title='Add DVD' className='btn btn-default navbar-btn navbar-right' onClick={addDvd}>
+          <span className='glyphicon glyphicon-cd' aria-hidden='true'></span>
       </button>
-      <button className='btn btn-default navbar-btn navbar-right' onClick={openQuickScan}>
+      <button title='Quick Scan' className='btn btn-default navbar-btn navbar-right' onClick={openQuickScan}>
         <span className='glyphicon glyphicon-barcode' aria-hidden='true'></span>
       </button>
 
