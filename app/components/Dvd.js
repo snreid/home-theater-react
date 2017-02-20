4a4d838e82205eb62cff620cf5ae64c800e50973
Sarah Reid <sreid@covermymeds.com>
2017-02-20 12:09:46 -0500

    Change table to list group

--- app/components/Dvd.js
+++ app/components/Dvd.js
@@ -1,28 +1,33 @@
 import React, { PropTypes } from 'react'
 
 const Dvd = ({ DVD_Title, DVD_ReleaseDate, Genre, location_id, locations, onDestroy, displayDvd, editDvd }) => (
-  <tr>
-    <td onClick={displayDvd}>{DVD_Title}</td>
-    <td onClick={displayDvd}>{DVD_ReleaseDate}</td>
-    <td onClick={displayDvd}>{Genre}</td>
-    <td onClick={displayDvd}>{locations.map(function(location){
-            if(location._id == location_id){
-              return location.display_name
-            }
-          }
-        )}
-    </td>
-    <td>
-      <button className='btn btn-danger' onClick={onDestroy}>
-        <span className='glyphicon glyphicon-trash' aria-hidden='true'></span>
-      </button>
-      &nbsp;
-      <button className='btn btn-primary' onClick={editDvd}>
-        <span className='glyphicon glyphicon-pencil' aria-hidden='true'></span>
-      </button>
-    </td>
-  </tr>
-
+  <li className='list-group-item'>
+    <div className='row'>
+      <div className='col-md-9' onClick={displayDvd}>
+        <h4>
+          {`${DVD_Title} `}
+          <small>
+            {`${DVD_ReleaseDate}, `}
+            {locations.map(function(location){
+                  if(location._id == location_id){
+                    return location.display_name
+                  }
+                }
+            )}
+          </small>
+        </h4>
+      </div>
+      <div className='col-md-3'>
+        <button className='pull-right btn btn-danger' onClick={onDestroy}>
+          <span className='glyphicon glyphicon-trash' aria-hidden='true'></span>
+        </button>
+        &nbsp;
+        <button className='pull-right btn btn-primary' onClick={editDvd}>
+          <span className='glyphicon glyphicon-pencil' aria-hidden='true'></span>
+        </button>
+      </div>
+    </div>
+  </li>
 )
 
 Dvd.propTypes = {
--- app/components/DvdList.js
+++ app/components/DvdList.js
@@ -2,33 +2,22 @@ import React, { PropTypes } from 'react'
 import Dvd from './Dvd'
 
 const DvdList = ({ dvds, locations, onDestroy, displayDvd, editDvd }) => (
-  <table className='table list-group'>
-    <thead>
-      <tr>
-        <th>Title</th>
-        <th>Release Date</th>
-        <th>Genre</th>
-        <th>Location</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
-    <tbody>
-        { dvds.map(dvd=>
-            <Dvd
-              key={dvd._id}
-              {...dvd}
-              locations={locations}
-              onDestroy={() => onDestroy(dvd._id)}
-              displayDvd={() => displayDvd(dvd._id)}
-              editDvd={() => editDvd(dvd._id)}
-            />
-          )
-        }
-        { dvds.length == 0 &&
-          <tr><td colSpan='5'>Loading DVD Library...</td></tr>
-        }
-    </tbody>
-  </table>
+  <ul className='list-group'>
+    { dvds.map(dvd=>
+        <Dvd
+          key={dvd._id}
+          {...dvd}
+          locations={locations}
+          onDestroy={() => onDestroy(dvd._id)}
+          displayDvd={() => displayDvd(dvd._id)}
+          editDvd={() => editDvd(dvd._id)}
+        />
+      )
+    }
+    { dvds.length == 0 &&
+      <li className='list-group-item' colSpan='5'>Loading DVD Library...</li>
+    }
+  </ul>
 )
 
 DvdList.propTypes = {
