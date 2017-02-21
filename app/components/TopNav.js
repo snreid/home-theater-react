d2d68454201b93095432e0611403e2b7619ac025
Sarah Reid <sreid@covermymeds.com>
2017-02-21 08:48:53 -0500

    Move AddDvd to modal

--- app/components/TopNav.js
+++ app/components/TopNav.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import QuickScanDvd from '../containers/QuickScanDvd'
+import AddDvd from '../containers/AddDvd'
 
 global.jQuery = global.$ = require('jquery')
 const bootstrap = require('bootstrap')
@@ -8,10 +9,16 @@ const TopNav = ({ addDvd, openQuickScan }) => (
   <div>
     <nav id='main-nav' className='navbar navbar-default navbar-fixed-top'>
       <div className='container'>
-        <button title='Add DVD' className='btn btn-default navbar-btn navbar-right' onClick={addDvd}>
+        <button title='Add DVD'
+                className='btn btn-default navbar-btn navbar-right'
+                data-toggle='modal'
+                data-target='#addDvdModal'>
             <span className='glyphicon glyphicon-cd' aria-hidden='true'></span>
         </button>
-        <button title='Quick Scan' className='btn btn-default navbar-btn navbar-right' data-toggle='modal' data-target='#myModal'>
+        <button title='Quick Scan'
+                className='btn btn-default navbar-btn navbar-right'
+                data-toggle='modal'
+                data-target='#quickScanModal'>
           <span className='glyphicon glyphicon-barcode' aria-hidden='true'></span>
         </button>
 
@@ -28,6 +35,7 @@ const TopNav = ({ addDvd, openQuickScan }) => (
 
 
     <QuickScanDvd />
+    <AddDvd />
 
   </div>
 )
--- app/containers/QuickScanDvd.js
+++ app/containers/QuickScanDvd.js
@@ -10,17 +10,58 @@ const QuickScanDvdComponent = ({dispatch, shouldDisplay, locations}) => {
   let location_node
 
   return(
-    <div className='modal fade' id='myModal' tabIndex='-1' role='dialog' aria-labelledby='myModalLabel'>
+    <div className='modal fade' id='quickScanModal' tabIndex='-1' role='dialog' aria-labelledby='quickScanModalLabel'>
       <div className='modal-dialog' role='document'>
         <div className='modal-content'>
           <div className='modal-header'>
             <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 className="modal-title" id="myModalLabel">Modal title</h4>
+            <h4 className="modal-title" id="quickScanModalLabel">Quick Scanner</h4>
           </div>
 
           <div className='modal-body'>
-            This is the body of the modal. Pretty cool, huh?
+						<form onSubmit={e => {
+							e.preventDefault()
+							if (!upc_node.value.trim()) {
+								return
+							}
+							var params = {UPC: upc_node.value, location_id: location_node.value}
+							dispatch(addDvdFromHomeTheaterInfo(params))
+							upc_node.value = ''
+						}}>
+							<div className='form-group'>
+								<label>UPC: </label>
+								<input className='form-control'
+												ref={node => {
+													upc_node = node
+												}}
+								/>
+							</div>
+							<div className='form-group'>
+								<label>Location: </label>
+								<select className='form-control'
+												ref={node => {
+													location_node= node
+												}}
+								>
+									{locations.map(function(location){
+										return(
+											<option key={location._id} value={location._id}>
+												{location.display_name}
+											</option>
+										)
+										}
+									)}
+								</select>
+							</div>
+							<button className="btn btn-success" type="submit">
+								Add DVD
+							</button>
+						</form>
           </div>
+
+					<div className='modal-footer'>
+						<button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
         </div>
       </div>
     </div>
