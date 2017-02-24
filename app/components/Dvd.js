d676e27fdba275c50e2ff5597154286a706d009e
Sarah Reid <sreid@covermymeds.com>
2017-02-24 09:33:57 -0500

    Display year of release

--- app/components/Dvd.js
+++ app/components/Dvd.js
@@ -3,26 +3,27 @@ import EditDvd from '../containers/EditDvd'
 import DvdDetails from '../containers/DvdDetails'
 
 const Dvd = ({ dvd, locations, onDestroy }) => {
+  let year = (isNaN(parseInt(dvd.DVD_ReleaseDate)) ? dvd.DVD_ReleaseDate : parseInt(dvd.DVD_ReleaseDate))
 
   return(
     <li className='list-group-item'>
       <div className='row'>
-        <div className='col-md-6'>
+        <div className='col-md-9'>
           <h4>
             {`${dvd.DVD_Title} `}
             <br/>
             <small>
-              {`${dvd.DVD_ReleaseDate}; `}
+              {`Released in ${year}; `}
               {locations.map(function(location){
                     if(location._id == dvd.location_id){
-                      return location.display_name
+                      return `Located on ${location.display_name}`
                     }
                   }
               )}
             </small>
           </h4>
         </div>
-        <div className='col-md-6'>
+        <div className='col-md-3'>
           <div>
             <button className='pull-right btn btn-danger' onClick={onDestroy}>
               <span className='glyphicon glyphicon-trash' aria-hidden='true'></span>
