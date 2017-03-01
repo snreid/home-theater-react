5d641b958af6a6dd07e036cd9bedbd352a831b1d
Sarah Reid <sreid@covermymeds.com>
2017-03-01 09:20:12 -0500

    Split string to get OR expression

--- app/mixins/Escaper.js
+++ app/mixins/Escaper.js
@@ -1,3 +1,8 @@
 export function escapeRegExp(string){
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
+
+export function splitTerms(string){
+  var new_string = string.split(' ')
+  return new_string.join('|')
+}
