cecd71723078965c971bd8f5d633c5413f04e573
Sarah Reid <sreid@covermymeds.com>
2017-03-03 09:07:38 -0500

    Export DVDS with location info

--- app/exportLibrary.js
+++ app/exportLibrary.js
@@ -1,27 +1,36 @@
 import { all_dvds } from './persist/Dvd'
+import { all_locations } from './persist/Location'
 
 const { dialog } = window.require('electron').remote
 var fs = window.require('fs')
 var json2csv = require('json2csv')
 
 
-var fields = ['DVD_Title', 'DVD_ReleaseDate', 'Genre', 'UPC', 'notes']
+var fields = ['DVD_Title', 'DVD_ReleaseDate', 'Genre', 'UPC', 'notes', 'location']
 
 export function exportCsv(){
   var answer = dialog.showOpenDialog({
     properties: ['openDirectory']
   })
 
-  console.log(answer)
-
   all_dvds().then(function(dvds){
-    var result = json2csv({data: dvds, fields: fields})
+    all_locations().then(function(locations){
+      var dvds_with_locations = dvds.map(function(dvd){
+        var location = locations.filter(function(location){
+          return location._id == dvd.location_id
+        })
+        var location_name = (location.length > 0) ? location[0].display_name : ''
+        return Object.assign({location: location_name}, dvd)
+      })
 
-    fs.writeFile(answer[0] + '/my-dvd-library.csv', result, function(err){
-      if(err){
-        return console.log(err)
-      }
-      console.log('file was saved!')
+      var result = json2csv({data: dvds_with_locations, fields: fields})
+
+      fs.writeFile(answer[0] + '/my-dvd-library.csv', result, function(err){
+        if(err){
+          return console.log(err)
+        }
+        console.log('file was saved!')
+      })
     })
   })
 
